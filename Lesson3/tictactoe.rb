734027557d156c23933a98dc9c71da1a819ebdec
Stephen Taylor <stephenmtaylor93@gmail.com>
2023-09-21 10:53:29 -0400

    more progress

--- Lesson3/tictactoe.rb
+++ Lesson3/tictactoe.rb
@@ -4,13 +4,30 @@ INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
+
+# get up to the last element in the array and join them
+# append the word or/and and append the last element in the array to string
+def joinor(squares, separator=', ', conj='or')
+  case squares.size
+  when 0 then ''
+  when 1 then squares.first.to_s
+  when 2 then squares.join(" #{conj} ")
+  else
+    output = squares[0..-2].join(separator) 
+    "#{output}#{separator}#{conj} #{squares[-1]}"
+  end
+end
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop: disable Metrics/AbcSize
 def display_board(brd)
-  system 'clear'
+  # system 'clear'
   puts "You're an #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}"
   puts ""
   puts "     |     |"
@@ -26,21 +43,22 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop: enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
-  new_board
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board 
 end
 
 def empty_squares(brd)
-  brd.keys.select{|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
   square = ''
-  loop do 
-    prompt "Choose a square (#empty_squares(brd).join(',')}):"
+  loop do
+    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
@@ -63,32 +81,31 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
-                  [[1, 5, 9], [3, 5, 7]]              # diagonals
-
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-      brd[line[1]] == PLAYER_MARKER && 
-      brd[line[2]] == PLAYER_MARKER
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER && 
-      brd[line[1]] == COMPUTER_MARKER &&
-      brd[line[2]] == COMPUTER_MARKER
-      return 'Computer' 
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
     end
   end
   nil
 end
 
-loop do 
-board = initialize_board
+def tally_point(string, player_score)
+  player_score
+end
 
-
+player_score = 0
+computer_score = 0
+match_point = 5
 
 loop do
+  board = initialize_board
+  prompt "Your score is #{player_score}, the Computer's score is #{computer_score}"
+
+  loop do
     display_board(board)
-    
+
     player_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
 
@@ -98,12 +115,19 @@ loop do
 
   display_board(board)
 
+  winner = detect_winner(board)
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
+    prompt "#{winner} won!"
   else
     prompt "It's a tie!"
   end
 
+  player_score += 1 if winner == 'Player'
+  computer_score += 1 if winner =='Computer'
+
+  prompt "You won the game!" if player_score == match_point
+  prompt "The computer won the game!" if computer_score == match_point
+    
   prompt "Play again? (y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
