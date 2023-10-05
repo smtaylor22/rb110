b68326927bd1144067cf7c8314432b0c35c82f8e
Stephen Taylor <stephenmtaylor93@gmail.com>
2023-10-05 09:43:56 -0400

    add files

---
+++ Practice Problems/Easy4pp/pp3.rb
@@ -0,0 +1,57 @@
+=begin
+
+Problem:
+  
+
+  Input: two strings
+  Output: one string
+
+  Rules:
+    Explicit requirements:
+      - 
+    Implicit requirements:
+      - 
+
+Example/Test Cases: 
+
+Data strucure:
+ - 
+
+Algorithm:
+find longest string
+concat the shortest string onto the ongest string before and after it
+
+Tests:
+=end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+leap_year?(2016) == true
+leap_year?(2015) == false
+leap_year?(2100) == false
+leap_year?(2400) == true
+leap_year?(240000) == true
+leap_year?(240001) == false
+leap_year?(2000) == true
+leap_year?(1900) == false
+leap_year?(1752) == true
+leap_year?(1700) == false
+leap_year?(1) == false
+leap_year?(100) == false
+leap_year?(400) == true
