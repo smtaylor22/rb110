7fa4d73fe7945a4fcaf4012478b4cbac617adff7
Stephen Taylor <stephenmtaylor93@gmail.com>
2023-10-13 11:30:48 -0400

    practice problems

--- Practice Problems/Easy4pp/pp8.rb
+++ Practice Problems/Easy4pp/pp8.rb
@@ -37,18 +37,34 @@ DIGITS = {
   '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
 }
 
-def string_to_signed_integer(string)
-  if string.chr == '-' || string.chr == '+'
-    negative = true if string.chr == '-'
-    string = string[1..-1]
-  end
-
+def string_to_integer(string)
   digits = string.chars.map { |char| DIGITS[char] }
 
   value = 0
   digits.each { |digit| value = 10 * value + digit }
-  value *= -1 if negative
   value
+end
+
+# def string_to_signed_integer(string)
+#   if string.chr == '-' || string.chr == '+'
+#     negative = true if string.chr == '-'
+#     string = string[1..-1]
+#   end
+
+#   digits = string.chars.map { |char| DIGITS[char] }
+
+#   value = 0
+#   digits.each { |digit| value = 10 * value + digit }
+#   value *= -1 if negative
+#   value
+# endp
+
+def string_to_signed_integer(string)
+  case string[0]
+  when '-' then -string_to_integer(string[1..-1])
+  when '+' then string_to_integer(string[1..-1])
+  else          string_to_integer(string)
+  end
 end
 
 p string_to_signed_integer('4321') == 4321
