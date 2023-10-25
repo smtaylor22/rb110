9724eec08803579cc34dcffc93a8706ef695f7ef
Stephen Taylor <stephenmtaylor93@gmail.com>
2023-10-25 11:34:47 -0400

    fibonaci

--- practice_problems/easy6/3.rb
+++ practice_problems/easy6/3.rb
@@ -1,18 +1,19 @@
 =begin
 
 Problem:
-  Input:
-  Output:
+  Input: Integer representing the first fibonaci number with the input parameter's number of digits
+  Output: Integer representing the index of that fibonaci number (starting with integer 1)
 
   Rules
     Implicit:
     Explicit:
 
 Examples/Test Cases:
-
+(2) == 7          # 1 1 2 3 5 8 13
+(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
 
 Data Structure:
-
+Array for the fibonacci sequence
 
 Algorithm:
 
