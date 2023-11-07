4102fc198762fd7ec48704a79fe26e94abe0e924
Stephen Taylor <stephenmtaylor93@gmail.com>
2023-11-07 11:35:08 -0500

    starting easy8

---
+++ practice_problems/easy8/1.rb
@@ -0,0 +1,19 @@
+=begin
+  
+Problem:
+  Input: array of integers
+  Output: sum of sums 
+
+  Rules:
+    Implicit:
+    Explicit: assume always be given array with 1 integer
+
+  Examples/Test Cases:
+  [3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+
+  Data Structure:
+
+  Algorithm:
+    
+
+=end
