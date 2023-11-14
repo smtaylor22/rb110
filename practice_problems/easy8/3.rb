4e1a232b0451524d1c16f9c3c55464db43314758
Stephen Taylor <stephenmtaylor93@gmail.com>
2023-11-14 08:09:04 -0500

    lastesgt

---
+++ practice_problems/easy8/3.rb
@@ -0,0 +1,33 @@
+=begin
+  
+Problem:
+  Input:
+  Output:
+
+  Rules:
+    Implicit:
+    Explicit:
+
+Examples/Test Cases:
+
+
+Data Structure:
+
+
+Algorithm:
+
+
+
+
+=end
+
+def substrings()
+
+
+substrings('abcde') == [
+  'a', 'ab', 'abc', 'abcd', 'abcde',
+  'b', 'bc', 'bcd', 'bcde',
+  'c', 'cd', 'cde',
+  'd', 'de',
+  'e'
+]
